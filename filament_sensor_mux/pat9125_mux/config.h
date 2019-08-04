934a019d51d46e608eafa6db83c22b1649282fb5
PetrNovacek <39201428+r21m@users.noreply.github.com>
2019-08-04 23:09:59 +0500

    Add files via upload

--- filament_sensor_mux/pat9125_mux/config.h
+++ filament_sensor_mux/pat9125_mux/config.h
@@ -1,1 +1,23 @@
-
+#ifndef CONFIG_H
+#define CONFIG_H
+//pat setting
+#define ADR 0x75
+#define TCA_RST_PIN 16
+#define XRES 0
+#define YRES 255
+//finda setting
+#define FINDA_PIN0 4
+#define FINDA_PIN1 5
+#define FINDA_PIN2 6
+#define FINDA_PIN3 7
+#define FINDA_PIN4 8
+#define FINDA_PIN5 9
+//
+#define INVERSE_FINDA 1
+#define PULLUP_FINDA 1
+//serial setup
+#define SERIAL_SPEED 115200
+//diag and mode
+#define DIAG_LED_PIN 15
+#define MODE_PIN 14
+#endif
---
+++ filament_sensor_mux/pat9125_mux/pat9125_mux.ino
@@ -0,0 +1,120 @@
+//
+#include "pat9125.h"
+#include "config.h"
+//
+
+#define unit_count 6
+const byte show = 2;
+
+//
+PAT9125 PAT[unit_count] = {
+  PAT9125(ADR, 0),
+  PAT9125(ADR, 1),
+  PAT9125(ADR, 2),
+  PAT9125(ADR, 3),
+  PAT9125(ADR, 4),
+  PAT9125(ADR, 5)
+};
+
+byte FINDA_PIN[unit_count] = {
+  FINDA_PIN0,
+  FINDA_PIN1,
+  FINDA_PIN2,
+  FINDA_PIN3,
+  FINDA_PIN4,
+  FINDA_PIN5
+};
+//
+byte finda_in[unit_count] = {};      //binary finda
+long x_value[unit_count]   = {};     //
+long y_value[unit_count]   = {};     //
+//int x2_value[unit_count]  = {};    //
+//int y2_value[unit_count]  = {};    //
+int s_value[unit_count]   = {};      //shutter value
+int b_value[unit_count]   = {};
+//
+
+void setup() {
+  pinMode(MODE_PIN, INPUT);
+  pinMode(DIAG_LED_PIN, OUTPUT);
+    
+  Serial.begin(SERIAL_SPEED);
+  delay(1000);
+  init_all();
+  set_res_all();
+  Serial.println("start");
+}
+
+void init_all() {
+  for (int i = 0; i < unit_count; i++) {
+    PAT[i].pat9125_init();
+    pinMode(FINDA_PIN[i], INPUT);
+#if PULLUP_FINDA
+    digitalWrite(FINDA_PIN[i], HIGH);
+#endif
+    delay(10);
+  }
+
+}
+
+void set_res_all() {
+  for (int i = 0; i < unit_count; i++) {
+    PAT[i].pat9125_set_res(XRES, YRES);
+    delay(10);
+  }
+}
+
+void update_all() {
+  //read PAT
+  for (int i = 0; i < unit_count; i++) {
+    PAT[i].pat9125_update();
+//    PAT[i].pat9125_update_x2();
+//    PAT[i].pat9125_update_y2();
+#if INVERSE_FINDA
+    finda_in[i] = digitalRead(FINDA_PIN[i]);
+#else
+    finda_in[i] = !digitalRead(FINDA_PIN[i]);
+#endif
+    delay(1);
+  }
+
+}
+
+void to_array() {
+  for (int i = 0; i < unit_count; i++) {
+    x_value[i]  = PAT[i].pat9125_x;
+    y_value[i]  = PAT[i].pat9125_y;
+//    x2_value[i] = PAT[i].pat9125_x2;
+//    y2_value[i] = PAT[i].pat9125_y2;
+    s_value[i]  = PAT[i].pat9125_s;
+    b_value[i]  = PAT[i].pat9125_b;
+  }
+}
+
+void serial_out_ascii() {
+
+  for (int i = 0; i < show; i++) {
+
+    Serial.print(i);
+    Serial.print(",");
+    Serial.print(finda_in[i]);
+    Serial.print(",");
+    Serial.print(x_value[i]);
+    Serial.print(",");
+    Serial.print(y_value[i]);
+    Serial.print(",");
+    Serial.print(b_value[i]);
+    Serial.print(",");
+    Serial.println(s_value[i]);
+    delay(1);
+  }
+}
+
+void loop() {
+
+  update_all();
+  to_array();
+  delay(100);
+  serial_out_ascii();
+
+}
